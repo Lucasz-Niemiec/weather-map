a149bb0ec06d7ed8bbec84f041a09f303a402f70
Lucasz-Niemiec <lucasniemieczenarruza21@gmail.com>
2023-01-01 14:37:46 -0300

    canvasadded

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const date = document.querySelector(".date") 
-// date seter function
-const changingName = () => {
-  date.textContent= "today's date"
-}
+//
+const myMap = document.getElementById("main-container");
+const date = document.querySelector(".date");
+const parent = myMap.parentNode;
+const imgContainer = document.getElementById("img-container");
+const img = document.createElement("img");
 
-changingName()
+// setting the date
+const changingName = () => {
+	date.textContent = "today's gato";
+};
+
+changingName();
+//adding style to image
+img.classList.add("imgMAp");
+//creating the canvas image
+html2canvas(myMap).then(function (canvas) {
+	img.src = canvas.toDataURL();
+	imgContainer.appendChild(img);
+});
+//removing the html elements to avoid duplicates
+parent.removeChild(myMap);
+html2canvas(myMap).then(function (canvas) {
+	parent.appendChild(myMap);
+});
+
+console.log(myMap, "here");
